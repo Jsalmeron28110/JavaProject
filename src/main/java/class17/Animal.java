2c679209e5d481822ba6b352f4345a0658fee0db
Jsalmeron28110 <juan.salmeron28110@gmail.com>
2023-09-05 22:56:32 -0400

    Class 17

---
+++ src/main/java/class17/Animal.java
@@ -0,0 +1,13 @@
+package class17;
+
+public class Animal {
+
+    String name;
+    String color;
+    String breed;
+    int age;
+
+    void printInfo(){
+        System.out.println(name+" "+breed+" "+color+" "+age);
+    }
+}
---
+++ src/main/java/class17/E.java
@@ -0,0 +1,18 @@
+package class17;
+
+public class E {
+   static String name="Juan";
+     static void printF(){
+         System.out.println(name);
+
+    }
+}
+
+class F extends E{
+    public static void main(String[] args) {
+        printF();
+    }
+
+}
+
+
